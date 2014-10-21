dbfcd25b7e0dfa48dcb53589ac5b999cf11f7647
jackywang135 <jackywang@KKBOXdeiMac.local>
2014-10-21 15:09:19 +0800

    Finished snake move, growhead/tail methods and testCases

--- AppleAndSnakeTests/AppleAndSnakeTests.swift
+++ AppleAndSnakeTests/AppleAndSnakeTests.swift
@@ -15,7 +15,7 @@ class AppleAndSnakeTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-//        snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
         
     }
     
@@ -29,33 +29,104 @@ class AppleAndSnakeTests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
+    func testSnakeInitAssert () {
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+    }
+    
     func testsnakeCanWithParameters() {
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
         XCTAssertNotNil(snake, "Should be able to init snake")
     }
     
     func testSnakeGetWidth(){
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
-        XCTAssertEqual(CGFloat(8), snake.width, "Width from snake incorrect")
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        XCTAssertEqual(16, snake.width, "Width from snake incorrect")
     }
     
     func testAccessSnakeHead() {
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
-        XCTAssertEqual(snake.snakeHead, CGRectMake(100, 100, 8, 8), "Should be able to get head")
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        XCTAssertEqual(snake.head, CGRectMake(100, 100, 16, 16), "Should be able to get head")
     }
     
     func testGetDirection() {
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
         XCTAssertEqual(snake.currentDirection, Direction.south, "Direction not correct")
     }
 
     func testSetLengthSmallerThan2() {
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
-        XCTAssertEqual(snake.length, 2, "Length should be set to 2")
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        XCTAssertEqual(snake.length, 3, "Length should be set to 3")
     }
     
     func testSnakeWidthSmallerThan8 (){
-        var snake : Snake = Snake(length:1, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 5, 5))
-        XCTAssertEqual(snake.width, CGFloat(8), "Width should be set to 8")
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        XCTAssertEqual(snake.width, 16, "Width should be set to 16")
+    }
+    
+    func testSnakeChangeInvalidDirection () {
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        snake.changeDirection(.north)
+        XCTAssertEqual(snake.currentDirection, Direction.south, "Direction did not change")
+    }
+    
+    func testSnakeChangeValidDirection () {
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        snake.changeDirection(.east)
+        XCTAssertEqual(snake.currentDirection, Direction.east, "Direction did not change")
+    }
+    
+    func testSnakeBodyCreateAfterInit () {
+        var snake : Snake = Snake(length:3, direction:Direction.south, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        XCTAssertEqual(snake.body.count, 3, "body not equal to length")
+    }
+    
+    func testSnakeBodyInCorrectionDirection () {
+        var snake : Snake = Snake(length:3, direction:Direction.north, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        let snakeBody1 = snake.body[1]
+        XCTAssertEqual(snakeBody1, CGRectMake(100, 116, 16, 16), "Body in wrong direction")
+        
+        let snakeBody3 = snake.body[2]
+        XCTAssertEqual(snakeBody3, CGRectMake(100, 132, 16, 16), "Body in wrong direction")
+    }
+    
+    func testSnakeMoveNorth() {
+        var snake : Snake = Snake(length:3, direction:Direction.north, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        snake.move()
+        let snakeHead = snake.head
+        XCTAssertEqual(snakeHead, CGRectMake(100, 84, 16, 16), "Snake didn't move")
+    }
+    
+    func testSnakeMoveEast() {
+        var snake : Snake = Snake(length:3, direction:Direction.east, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        snake.move()
+        let snakeHead = snake.head
+        XCTAssertEqual(snakeHead, CGRectMake(116, 100, 16, 16), "Snake didn't move")
+    }
+    
+    func testSnakeMoveLengthConsistent() {
+        var snake : Snake = Snake(length:3, direction:Direction.east, snakeHeadRect:CGRectMake(100, 100, 16, 16))
+        snake.move()
+        snake.move()
+        snake.move()
+        snake.move()
+        snake.move()
+        snake.move()
+        XCTAssertEqual(snake.body.count, 3, "Length not consistent after move")
+    }
+    
+    func testSnakeGrowHead () {
+        var snake : Snake = Snake(length:3, direction:Direction.east, snakeHeadRect:CGRectMake(100, 100, 32, 32))
+        snake.growHead()
+        XCTAssertEqual(snake.length, 4, "Length did not grow")
+        XCTAssertEqual(snake.head.origin, CGPointMake(132, 100), "Did not grow at correct Point")
+    }
+    
+    func testSnakeGrowTail () {
+        var snake : Snake = Snake(length:3, direction:Direction.east, snakeHeadRect:CGRectMake(100, 100, 32, 32))
+        snake.growHead()
+        snake.growHead()
+        XCTAssertEqual(snake.length, 5, "Length did not grow")
+        XCTAssertEqual(snake.head.origin, CGPointMake(164, 100), "Did not grow at correct Point")
+        
     }
 }
--- AppleAndSnakeTests/Snake.swift
+++ AppleAndSnakeTests/Snake.swift
@@ -11,47 +11,112 @@ import CoreGraphics
 import Darwin
 
 enum Direction {
-    case north, east, south, west
+    case  east, south, west, north
 }
 
 class Snake : NSObject{
     
-    
-    // MARK: Initializer
-    init(length:Int, direction:Direction, snakeHeadRect:CGRect){
-        if length < 2 {
-            self.length = 2
-        } else {
-            self.length = length
-        }
-        
-        self.currentDirection = direction
-        
-        if (snakeHeadRect.size.height < 8 || snakeHeadRect.size.width < 8) {
-            let snakeHeadPoint = snakeHeadRect.origin
-            let newSnakeHeadRect : CGRect = CGRectMake(snakeHeadPoint.x, snakeHeadPoint.y, 8, 8)
-            self.snakeBody.append(newSnakeHeadRect)
-        } else {
-            self.snakeBody.append(snakeHeadRect)
-        }
-    }
-    
     // MARK: Variables
-    var length : Int
-    var currentDirection : Direction!
-    var snakeHead : CGRect {
+    
+    var length : Int {
         get {
-            return self.snakeBody.first!
+            return self.body.count
         }
-
     }
-    var snakeBody = [CGRect]()
-    var width : CGFloat {
+    var currentDirection : Direction
+    var head : CGRect {
         get {
-            return self.snakeHead.size.width
+            return self.body.first!
+        }
+        
+    }
+    var body = [CGRect]()
+    var width : Int {
+        get {
+            return Int(self.head.size.width)
+        }
+    }
+
+    // MARK: Initializer
+    
+    init(length:Int, direction:Direction, snakeHeadRect:CGRect){
+   
+        self.currentDirection = direction
+        self.body.append(snakeHeadRect)
+        
+        assert(length > 2, "Length must be greater than 2")
+        assert(snakeHeadRect.size.height >= 8 || snakeHeadRect.size.width >= 8 || snakeHeadRect.size.height != snakeHeadRect.size.width , "SnakeHeadRect.height and .width must be greater than 8 and equal")
+        
+        //Create Body After Parameters given
+        func getWidth () -> Int {return Int(snakeHeadRect.size.width)}
+        for bodyIndex in 1...length-1 {
+    
+            var newBodyPoint : CGPoint?
+            var newBodyRect : CGRect?
+            
+            switch direction {
+            case .north:
+                newBodyPoint = CGPointMake(snakeHeadRect.origin.x, snakeHeadRect.origin.y + CGFloat(getWidth() * bodyIndex))
+            case .south:
+                newBodyPoint = CGPointMake(snakeHeadRect.origin.x, snakeHeadRect.origin.y - CGFloat(getWidth() * bodyIndex))
+            case .east:
+                newBodyPoint = CGPointMake(snakeHeadRect.origin.x - CGFloat(getWidth() * bodyIndex), snakeHeadRect.origin.y)
+            case .west:
+                newBodyPoint = CGPointMake(snakeHeadRect.origin.x + CGFloat(getWidth() * bodyIndex), snakeHeadRect.origin.y)
+            }
+            newBodyRect = CGRectMake(newBodyPoint!.x, newBodyPoint!.y, CGFloat(getWidth()), CGFloat(getWidth()))
+            self.body.append(newBodyRect!)
+        }
+    }
+
+
+
+    // MARK: Methods 
+    
+    func changeDirection(newDirection:Direction) {
+        switch newDirection {
+        case .north:
+            if self.currentDirection != .south { self.currentDirection = newDirection }
+        case .east:
+            if self.currentDirection != .west { self.currentDirection = newDirection }
+        case .south:
+            if self.currentDirection != .north { self.currentDirection = newDirection }
+        case .west:
+            if self.currentDirection != .east { self.currentDirection = newDirection }
         }
     }
     
-
+    func move() {
+        self.growHead()
+        self.body.removeLast()
+    }
+    
+    func growHead() {
+        var newSnakeHead : CGRect?
+        switch self.currentDirection {
+        case .north:
+            newSnakeHead = CGRectMake(self.head.origin.x, self.head.origin.y - CGFloat(self.width), CGFloat(self.width), CGFloat(self.width))
+        case .south:
+            newSnakeHead = CGRectMake(self.head.origin.x, self.head.origin.y + CGFloat(self.width), CGFloat(self.width), CGFloat(self.width))
+        case .east:
+            newSnakeHead = CGRectMake(self.head.origin.x + CGFloat(self.width), self.head.origin.y, CGFloat(self.width), CGFloat(self.width))
+        case .west:
+            newSnakeHead = CGRectMake(self.head.origin.x - CGFloat(self.width), self.head.origin.y, CGFloat(self.width), CGFloat(self.width))
+        }
+        self.body.insert(newSnakeHead!, atIndex: 0)
+    }
+    
+    func growTail() {
+        var snakeTail : CGPoint = self.body.last!.origin
+        var snakeSecondToLastTail : CGPoint = self.body[self.body.count-2].origin
+        let xDistance = snakeTail.x - snakeSecondToLastTail.x
+        let yDistance = snakeTail.y - snakeSecondToLastTail.y
+        
+        var newSnakeTail : CGRect = CGRectMake(snakeTail.x + xDistance, snakeTail.y + yDistance, CGFloat(self.width), CGFloat(self.width))
+        self.body.append(newSnakeTail)
+        
+        
+    }
+    
     
 }
