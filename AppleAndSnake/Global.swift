ac483b75b6e572f95efb397b71efbd91397b502e
Jacky Wang <Jacky@123-192-235-151.dynamic.kbronet.com.tw>
2014-12-16 09:22:15 -0800

    Added device recognition singleton in Global

--- AppleAndSnake/Global.swift
+++ AppleAndSnake/Global.swift
@@ -9,6 +9,34 @@
 import Foundation
 import UIKit
 
+enum DeviceType {
+    case iPhone4or4S, iPhone5or5S, iPhone6, iPhone6Plus, iPad, unspecified
+}
+
+var DeviceModel : DeviceType {
+    struct deviceSingleton {
+        static let type = getDeviceType()
+    }
+    return deviceSingleton.type
+}
+
+func getDeviceType() -> DeviceType {
+    
+    let iPhone6PlusScreenHeight = CGFloat(736)
+    let iPhone6ScreenHeight = CGFloat(667)
+    let iPhone5ScreenHeight = CGFloat(568)
+    let iPhone4ScreenHeight = CGFloat(480)
+    
+    switch screenHeight {
+    case iPhone4ScreenHeight: return .iPhone4or4S
+    case iPhone5ScreenHeight: return .iPhone5or5S
+    case iPhone6ScreenHeight: return .iPhone6
+    case iPhone6PlusScreenHeight: return .iPhone6Plus
+    case let x where screenHeight > iPhone6PlusScreenHeight : return .iPad
+    default : return .unspecified
+    }
+}
+
 let screenHeight = UIScreen.mainScreen().bounds.height
 let screenWidth = UIScreen.mainScreen().bounds.width
 
